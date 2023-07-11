167ac16505409d30c1617c245557c4e9f3a966c9
Matt <77928207+mattzcarey@users.noreply.github.com>
2023-07-11 17:28:46 +0100

    feat: init

---
+++ index.ts
@@ -0,0 +1,35 @@
+const express = require("express");
+const crypto = require("crypto");
+const fs = require("fs");
+
+const app = express();
+
+const SECRET_KEY = "my-secret-key";
+
+app.get("/fib", (req, res) => {
+  const num = req.query.num;
+
+  let a = 0;
+  let b = 1;
+  let c = 0;
+
+  for (let i = 2; i <= num; i++) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+
+  const encrypted = crypto
+    .createHmac("sha256", SECRET_KEY)
+    .update(b.toString())
+    .digest("hex");
+
+  fs.writeFileSync("output.txt", encrypted);
+
+  res.send(encrypted);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+  console.log(`SECRET_KEY is: ${SECRET_KEY}`);
+});
